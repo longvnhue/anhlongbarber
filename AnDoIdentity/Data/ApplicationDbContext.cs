4da391d1d05f16de94e8e1ca1ebf4cea1981b05d
longvnhue <hj3u126@gmail.com>
2023-05-01 22:21:35 +0700

    anhlongbarber

---
+++ AnDoIdentity/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AnDoIdentity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AnDoIdentity.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { 
+            
+        }
+        
+        public DbSet<Item> Items { get; set; }
+    }
+}
